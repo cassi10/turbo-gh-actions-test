237bd3dab410c54469c722c10db0c771468625b8
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-04 05:52:46 +0100

    updated ci workflow file

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@scope/ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docsssssdfsdfdsfsd</h1>
+      <h1>Docsssssdfsdfdsfsda</h1>
       <Button />
     </div>
   );
