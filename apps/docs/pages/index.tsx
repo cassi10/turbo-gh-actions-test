f48d33a7c93d0dcdd312daca50fcc62982c0bd4f
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-05 02:37:41 +0100

    updated docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,9 @@ import { Button } from "@scope/ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docsssssdfsdfdsfsda testyyy</h1>
+      <h1>
+        Docsssssdfsdfdsfsda testyyy this should trigger the docs to deploy.
+      </h1>
       <Button />
     </div>
   );
