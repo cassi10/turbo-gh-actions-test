204c166e33e78445ca1075453c9dbeba90ebbf3e
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-05 01:37:11 +0100

    updated docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@scope/ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docsssssdfsdfdsfsda a</h1>
+      <h1>Docsssssdfsdfdsfsda</h1>
       <Button />
     </div>
   );
