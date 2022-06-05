3d201efdbebcc3ed2723fd753e6a3751dbe49845
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-05 02:48:04 +0100

    updated docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Docs() {
   return (
     <div>
       <h1>
-        Docsssssdfsdfdsfsda testyyy this should trigger the docs to deploy.
+        Docsssssdfsdfdsfsda testyyy this should trigger the docs to deploy. oops
       </h1>
       <Button />
     </div>
