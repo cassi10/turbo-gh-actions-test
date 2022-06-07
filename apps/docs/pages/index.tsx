ec5bb461fb1d98fb03074243907ac711a3b9e694
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-07 21:17:31 +0100

    updated files

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,13 +3,7 @@ import { Button } from "@scope/ui";
 export default function Docs() {
   return (
     <div>
-      <h1>
-        Docsssssdfsdfdsfsda testyyy this should trigger the docs to deploy. oops
-        Jesus H. Christ
-      </h1>
-      <h6>Maybe it will work this time...</h6>
-      <h6>I have no idea what is happening...</h6>
-      <h6>this should only show on staging.testing.ibcarr.com</h6>
+      <h1>DOCS</h1>
       <Button />
     </div>
   );
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@scope/ui";
 export default function Web() {
   return (
     <div>
-      <h1>Webbyyytttfdfds nexty</h1>
+      <h1>WEB</h1>
       <Button />
     </div>
   );
