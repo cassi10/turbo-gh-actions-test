0c7f796b08b93cb01b764f6a65ddbb8747689321
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-11 02:48:46 +0100

    updated files

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -6,8 +6,6 @@ export default function Docs() {
       <h1>DOCS</h1>
       <h5>Next</h5>
       <h5>Next again</h5>
-      <h5>Maybe once more</h5>
-      <h5>Okay. Please...</h5>
       <Button />
     </div>
   );
