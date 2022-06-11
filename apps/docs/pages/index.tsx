728391266920de18a1ce02a177460bd84009e94d
Isaac <67207048+cassi10@users.noreply.github.com>
2022-06-11 05:21:49 +0100

    other misc changes

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,10 +4,6 @@ export default function Docs() {
   return (
     <div>
       <h1>DOCS</h1>
-      <h5>Next</h5>
-      <h5>Next again</h5>
-      <h5>again</h5>
-      <h6>what the frickity frack</h6>
       <Button>Docs button</Button>
     </div>
   );
